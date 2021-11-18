738f70961cb0baab31a3f33eb8cf3299d287ce84
kalus <kaluski1212@wp.pl>
2021-11-18 18:52:44 +0100

    Change CLI structure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import click
+import FileManagers.PCAPManager as pcap_manager
+import FileManagers.FileManager as file_manager
 
 @click.group()
 def main():
@@ -18,16 +20,39 @@ def select_mode(count, name):
         click.echo(f"Hello {name}!")
 
 @main.command()
-def scan_folder():
-    folder_name = click.prompt('Foldername to scan', type=str)
-    click.echo(f"Folder{folder_name} scanned")
+@click.option('--folder-name', help='Name of the folder to scan')
+def scan_folder(folder_name):
+    click.echo(f"Folder {folder_name} scanned")
+    data = folder_name
 
 
 @main.command()
-def scan_file():
-    file_name = click.prompt('Filename to scan', type=str)
-    click.echo(f"Folder {file_name} scanned")
+@click.option('--file-name', help='Name of the folder to scan')
+def scan_file(file_name):
+    click.echo(f"File {file_name} scanned")
+    data = file_name
+
+@main.command()
+@click.option('--folder-name', default=None, help='Folder name to scan')
+@click.option('--file-name', default=None, help='Folder name to scan')
+@click.option('--load-rules', default=None, help='Folder name to scan')
+def test(folder_name, file_name, load_rules):
+    if(folder_name != None):
+        scan_folder
+    if (file_name != None):
+        scan_file
+
+
+
+@main.command()
+@click.option('--rules', help='Name of the folder to scan')
+def load_rules(rules):
+    for r in rules:
+        print(r)
+
 
 
 if __name__ == '__main__':
-    main()
+    #global data
+    #main()
+    file_manager.get_filenames(r"C:\Users\kalus\OneDrive\Pulpit\BACKUP2-UAIM")
