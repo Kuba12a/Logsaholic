9bab94b73988771b827a4f7e1635886a71fe0733
MarcinDydo <01149354@pw.edu.pl>
2021-11-20 08:32:44 -0500

    validation + txt search

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import click
 import os
-import scapy
 import FileManagers.PCAPManager as pcap_manager
 import FileManagers.FileManager as file_manager
 import FileManagers.EventDetectionManager as event_manager
+import FileManagers.TXTManager as txt_manager
 
 #main groupf to serve multiple commands 
 @click.group()
@@ -43,38 +43,43 @@ def event_detection(path, rules):
 def display_captures(path, filter):
     """ Simple programm to display PCAPs """
     
-    # jakaś walidacja czy to dobry filtr BPF
-    try:
-        if os.path.isfile(path):
-            click.echo("PCAPs from folder "+ path + " displayed using " + filter+ " filter")
-            result = pcap_manager.process_pcap(path, filter)
-            result.summary()
-    except Exception as ex:
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print(message)
+    if pcap_manager.validate_filter(filter):
+        try:
+            if os.path.isfile(path):
+                click.echo("PCAPs from folder "+ path + " displayed using " + filter+ " filter")
+                result = pcap_manager.process_pcap(path, filter)
+                pcap_manager.display_pkts(result)
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+    else: click.echo("invalid filter")
+
+    
 
 
 
 @main.command()
 @click.option('--path', default=[], help='Path to folders or files', multiple=True)
-@click.option('--regex', default=None, help='Valid regular-expression')
+@click.option('--regex', help='Valid regular-expression in single quotes', required=True)
 def text_search(path, regex):
     """ Simple programm to search for text """
     # jakaś walidacja czy to dobre regullar expression
-    try:
-        files_to_scan = []
-        for p in path:
-            if os.path.isdir(p):
-                for file in file_manager.get_filenames(p): #TODO naprawic 
-                    files_to_scan.append(file)
-            elif os.path.isfile(p):
-                files_to_scan.append(p)
-    except Exception as ex:
-        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print(message)
-
+    if txt_manager.validate_regex(regex):
+        try:
+            files_to_scan = []
+            for p in path:
+                if os.path.isdir(p):
+                    for file in file_manager.get_filenames(p): #TODO naprawic 
+                        files_to_scan.append(file)
+                elif os.path.isfile(p):
+                    files_to_scan.append(p)
+            txt_manager.process_files(files_to_scan,regex)
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+    else: click.echo("regex incorrect")
 
 
 
