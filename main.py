9391c4d8f29ea3e94d89ed1c516f40d5ba1d9b29
MarcinDydo <01149354@pw.edu.pl>
2021-11-24 14:04:22 -0500

    repaired walks

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ def main():
 @click.option('--rules', prompt="enter rules >", help='Possible rules are:')
 def event_detection(path, rules):
     """ Event generating detection programm """
+    extensions = {'pcapng', 'txt', 'xml', 'json', 'evtx'}    
     try:
         rules = rules.split(" ")  #String table with rules names
         files_to_scan = []
         for p in path:
             if os.path.isdir(p):
-                for file in file_manager.get_filenames(p): #TODO naprawic 
+                for file in file_manager.get_filenames(p,extensions): #TODO naprawic 
                     files_to_scan.append(file)
             elif os.path.isfile(p):
                 files_to_scan.append(p)
@@ -64,13 +65,13 @@ def display_captures(path, filter):
 @click.option('--regex', help='Valid regular-expression in single quotes', required=True)
 def text_search(path, regex):
     """ Simple programm to search for text """
-    # jakaś walidacja czy to dobre regullar expression
+    extensions = {'txt', 'xml', 'json'}    
     if txt_manager.validate_regex(regex):
         try:
             files_to_scan = []
             for p in path:
                 if os.path.isdir(p):
-                    for file in file_manager.get_filenames(p): #TODO naprawic 
+                    for file in file_manager.get_filenames(p,extensions):
                         files_to_scan.append(file)
                 elif os.path.isfile(p):
                     files_to_scan.append(p)
