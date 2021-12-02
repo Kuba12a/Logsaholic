ea97e667fbaa51ff9a738fd9056430d8e6755aea
Szymon Miareczka <szymiar@gmail.com>
2021-12-02 16:06:26 +0100

    detect_malware_ip rules added

--- FileManagers/EventDetectionManager.py
+++ FileManagers/EventDetectionManager.py
@@ -9,8 +9,6 @@ detection_rules = __import__('detection-rules')
 
 
 def scan_files(files, rules, firewall_address, host_address):
-    print(files)
-    print(rules)
     event_number = 1
     log_message = []
     method = getattr(detection_rules, rules[0])
@@ -20,42 +18,38 @@ def scan_files(files, rules, firewall_address, host_address):
         for f in files:
             method = getattr(detection_rules, r)
             result = method(f)
-
+            if(result[0] is None):
+                continue
             log_message.append("Event number:" + str(event_number))
             log_message.append("Alert source : " + f)
             log_message.append("Alert rule : " + r)
             log_message.append("Alert type : " + result[0])
             log_message.append("Alert description : " + result[2])
-
             if (result[0] == "local"):  # wypisz na CLI, do loga i do SQL
                 db.insert_alert(db.AlertToInsert(source=f, date=datetime.datetime.now(), message=result[2]))
-                print("\n Added to sql database\n")
-                # save_to_sql_database
-
+        
             elif (result[0] == "remote"):  # wypisz na CLI, do loga, do SQL i wyśli do zdalnego hosta
                 db.insert_alert(db.AlertToInsert(source=f, date=datetime.datetime.now(), message=result[2]))
-                http_client.send_alert(host_address,
+                try:
+                    http_client.send_alert(host_address,
                                        alert_model.Alert(alert_id=event_number, source=f,
                                                          date=str(datetime.datetime.now()), message=result[2]))
-                print("\n Added to sql database and sent to remote host\n")
-                # save_to_sql_database
-                # send_description_to_remote(result[2])
+                except: 
+                    print("Nie wyslano do hosta odbierającego, bo nie jest on uruchomiony")
+
 
             if (result[1]):  # Jesli action_block = True
-                http_client.send_alert(firewall_address,
-                                       alert_model.Alert(alert_id=event_number, source=f,
-                                                         date=str(datetime.datetime.now()), message=result[2]))
+                try:
+                    http_client.send_alert(firewall_address,
+                                        alert_model.Alert(alert_id=event_number, source=f,
+                                                        date=str(datetime.datetime.now()), message=result[2]))
+                except:
+                    print("Nie wyslano polecen do firewalla bo serwer odbierajacy nie jest uruchomiony")
                 log_message.append("Sending firewall rules to remote host")
-                # send_firewall_rules_to_remote()
+
 
             log_message.append("")
             log_message.append("")
             event_number += 1
     pprint(log_message)  # Wyświetl na CLI
     file_manager.write_log(log_message)  # Zapis loga z przeprowadzonych skanow
-
-# except Exception as ex:
-
-# template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-# message = template.format(type(ex).__name__, ex.args)
-# print(message)
--- detection-rules.py
+++ detection-rules.py
@@ -1,8 +1,9 @@
 from scapy.all import *
 import Evtx.Evtx as evtx
+import re
 
 
-def detectMalwareName(file):    
+def detect_malware_name(file):    
     condition = False
     keys = ["powershell", "empire", "trojan"]
 
@@ -32,8 +33,8 @@ def detectMalwareName(file):
 
     
     if condition == True:
-        action_alert = "remote" # akcja: "local", "remote"
-        action_block = True # or False
+        action_alert = "local" # akcja: "local", "remote"
+        action_block = False # or False
         description = "Opis eventu, low risk" # format w OFF.8.5
     else:
         action_alert = None
@@ -43,8 +44,78 @@ def detectMalwareName(file):
 
 
 
-def detectMalwareURL(file):
-    condition = True
+def detect_malware_ip(file):
+    condition = False
+    ip_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')   #Ip regex
+    keys = ["666.666.666.666", "119.189.231.225", "43.128.202.179", "192.168.178.4"]
+    #procesowanie pcap
+    if(file.split(".")[1] == "pcapng"):
+        pkts = sniff(offline=file, pnr=None, filter='tcp port 80')
+        for pkt in pkts:
+            payload = pkt.sprintf("{Raw:%Raw.load%}\n")
+            for key in keys:
+                if key in payload: condition = True
+
+
+        '''
+        ips = []
+        u=1
+        for p in PcapNgReader(file):
+            print(p[IPField].dst)
+            if IPField in p:    
+                u+=1    
+                print(p[IPField].dst)      
+                ips.append(p[IPField].src)
+                ips.append(p[IPField].dst)
+            if u==5:
+                break
+        for ip in ips:
+            if ip in keys: 
+                condition = True
+                print(ip)
+            '''
+
+    #procesowanie evtx
+    elif(file.split(".")[1] == "evtx"):
+        #print("Weszlo do evtx")
+        with evtx.Evtx(file) as log:
+            for record in log.records():
+                payload = str(record.xml())
+                for key in keys:
+                    if key in payload: condition = True
+
+
+    #procesowanie xml, json i txt
+    elif(file.split(".")[1] in ["xml","json","txt"]):
+        #print("Wchodzi do txt")
+        result = []
+        with open(file, 'r') as f:
+            for line in f:
+                for l in ip_pattern.findall(line):
+                    result.append(l)
+
+            for key in keys:
+                if key in result: 
+                    condition = True
+
+    if condition==True:
+        action_alert = "remote" # akcja: "local", "remote"
+        action_block = True # or False
+        description = "Opis Eventu, high risk"  # format w OFF.8.5
+    else:
+        action_alert = None
+        action_block = None
+        description = None
+    return action_alert, action_block, description
+
+
+
+
+
+#TODO
+def detect_malware_url(file):
+    condition = False
+    keys = ["http://malware.com", "https://studia.elka.pw.edu.pl/", "http://virus.com"]
 
     if condition==True:
         action_alert = "remote" # akcja: "local", "remote"
@@ -54,31 +125,5 @@ def detectMalwareURL(file):
         action_alert = None
         action_block = None
         description = None
-    return action_alert, action_block, description\
-
-    
-def detectMalwareIP(file):
-    # ciało funkcji - właściwa reguła operująca na danych z args
-    # procesowanie pcap
-    # for pcap in kwargs[pcap]:
-    # procesowanie evtx
-    # for evtx in kwargs[evtx]:
-    # procesowanie xml
-    # for xml in kwargs[xml]:
-    # procesowanie json
-    # for json in kwargs[json]:
-    # procesowanie txt
-    # for txt in kwargs[txt]:
-    # ostateczna reguła - tj. co ma się wykonać
-
-    condition = True
-
-    if condition==True:
-        action_alert = "remote" # akcja: "local", "remote"
-        action_block = True # or False
-        description = "opis Eventu, high risk" # format w OFF.8.5
-    else:
-        action_alert = None
-        action_block = None
-        description = None
     return action_alert, action_block, description
+
