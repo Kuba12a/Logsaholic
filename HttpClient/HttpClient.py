48b395db9971725152c2b37030c98208afcebc89
kalus <kaluski1212@wp.pl>
2021-12-19 17:21:21 +0100

    Add http_prefix to HttpClient.py

--- HttpClient/HttpClient.py
+++ HttpClient/HttpClient.py
@@ -3,12 +3,14 @@ import Model.Alert as Alert
 import json
 import datetime
 
+http_prefix = "http://"
+
 
 def send_alert(URL, alert: Alert):
 
     data = json.dumps(alert.__dict__)
 
-    response = requests.post(url=URL, data=data)
+    response = requests.post(url=http_prefix+URL, data=data)
 
     # extracting data in json format
     status_code = response.status_code
